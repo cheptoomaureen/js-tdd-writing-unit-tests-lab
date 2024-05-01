36a1d1aef08ac7e92e6b51d81af55134a3c25257
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-05-01 15:37:47 +0300

    assignment

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,15 @@
 // Your tests here
+import { isPalindrome } from "../utils"; 
+
+describe("isPalindrome()", () => {
+    it("checks if word is a palindrome", () => {
+        const word = "racecar";
+        expect(isPalindrome(word)).toBe(true);
+    });
+
+    it("checks if word is not a palindrome", () => {
+        const word = "car";
+        expect(isPalindrome(word)).toBe(false);
+    });
+    const word = "test"
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
 // Your code here
+export function isPalindrome(word) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const processedWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    // Check if the processed word is equal to its reverse
+    return processedWord === processedWord.split('').reverse().join('');
+}
